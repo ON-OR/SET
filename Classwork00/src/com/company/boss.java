0135361ba04b3fe6eab3491278e0c28023b22ef9
Orest Onishchenko <30510240+ON-OR@users.noreply.github.com>
2019-10-21 15:54:26 +0300

    Update boss.java

--- Classwork00/src/com/company/boss.java
+++ Classwork00/src/com/company/boss.java
@@ -1,6 +1,6 @@
 package com.company;
 
-class Boss extends Employee {
+class Boss extends Employee {//subclass that inherits from Employee class
     public void funk(){//Function to output text.
         System.out.println("I am BOSS, \n");
     }
