2d9fc3be2d75df946203b7018e71d6cd3f173e1f
Orest Onishchenko <30510240+ON-OR@users.noreply.github.com>
2019-10-21 15:53:14 +0300

    Update Employee.java

--- Classwork00/src/com/company/Employee.java
+++ Classwork00/src/com/company/Employee.java
@@ -1,14 +1,14 @@
 package com.company;
 import java.awt.*;
 
-class Employee {
+class Employee {//Superclass 
     String scream = "hey, you, lazy potato, you  are fired!!!";
-    public void sound()
+    public void sound()//Function to output text & sound `bip`
         {
             System.out.println(scream);
             Toolkit.getDefaultToolkit().beep();
         }
-    public void soundloud()
+    public void soundloud()//Function to output CapsLk text & sound `bip`
     {
         System.out.println(scream.toUpperCase());
         Toolkit.getDefaultToolkit().beep();
