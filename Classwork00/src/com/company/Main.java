6b02b193dd12db95772eba8155f47a7520c46826
Orest Onishchenko <30510240+ON-OR@users.noreply.github.com>
2019-10-21 15:48:25 +0300

    Update Main.java

--- Classwork00/src/com/company/Main.java
+++ Classwork00/src/com/company/Main.java
@@ -2,11 +2,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Boss Obj = new Boss();
-        Obj.funk();
-        Obj.sound();
-        Bigboss Obj1 = new Bigboss();
-        Obj1.funkbig();
-        Obj1.soundloud();
+        Boss obj = new Boss();//Creating Boss Class object.
+        obj.funk();
+        obj.sound();
+        Bigboss obj1 = new Bigboss();//Creating Bigboss Class object.
+        obj1.funkbig();
+        obj1.soundloud();
     }
 }
