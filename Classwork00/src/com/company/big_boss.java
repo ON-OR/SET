ff07f06bc30d2ccc0214a2cfadd0452ea6ae381a
Orest Onishchenko <30510240+ON-OR@users.noreply.github.com>
2019-10-21 15:54:13 +0300

    Update big_boss.java

--- Classwork00/src/com/company/big_boss.java
+++ Classwork00/src/com/company/big_boss.java
@@ -1,6 +1,6 @@
 package com.company;
 
-class Bigboss extends Employee {
+class Bigboss extends Employee {//subclass that inherits from Employee class
     public void funkbig (){//Function to output text.
         System.out.println("I AM BIG BOSS, \n");
     }
